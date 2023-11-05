babe5a3e8d6f1b637ab4ae0259d8dca3f4e6f4bf
Debdipto Biswas <127871393+DebdiptoBiswas@users.noreply.github.com>
2023-11-05 17:57:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+for(var i = 0; i < document.querySelectorAll(".drum").length; i ++){
+	document.querySelectorAll(".drum")[i].addEventListener("click", fnc);
+
+}
+
+function fnc(){
+	if(this.innerHTML === "w"){
+		var audio = new Audio("./sounds/crash.mp3");
+		audio.play();
+	}
+	if(this.innerHTML === "a"){
+		var audio = new Audio("./sounds/kick-bass.mp3");
+		audio.play();
+	}
+	if(this.innerHTML === "s"){
+		var audio = new Audio("./sounds/snare.mp3");
+		audio.play();
+	}
+	if(this.innerHTML === "d"){
+		var audio = new Audio("./sounds/tom-1.mp3");
+		audio.play();
+	}
+	if(this.innerHTML === "j"){
+		var audio = new Audio("./sounds/tom-2.mp3");
+		audio.play();
+	}
+	if(this.innerHTML === "k"){
+		var audio = new Audio("./sounds/tom-3.mp3");
+		audio.play();
+	}
+	if(this.innerHTML === "l"){
+		var audio = new Audio("./sounds/tom-4.mp3");
+		audio.play();
+	}
+}
