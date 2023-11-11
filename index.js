396c601d7c0a7f72512a3653380cf86d14de8517
Debdipto Biswas <127871393+DebdiptoBiswas@users.noreply.github.com>
2023-11-11 12:21:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,3 +33,34 @@ function fnc(){
 		audio.play();
 	}
 }
+
+document.addEventListener("keydown",function(event){
+	if(event.key === "w"){
+		var audio = new Audio("./sounds/crash.mp3");
+		audio.play();
+	}
+	if(event.key === "a"){
+		var audio = new Audio("./sounds/kick-bass.mp3");
+		audio.play();
+	}
+	if(event.key === "s"){
+		var audio = new Audio("./sounds/snare.mp3");
+		audio.play();
+	}
+	if(event.key === "d"){
+		var audio = new Audio("./sounds/tom-1.mp3");
+		audio.play();
+	}
+	if(event.key === "j"){
+		var audio = new Audio("./sounds/tom-2.mp3");
+		audio.play();
+	}
+	if(event.key === "k"){
+		var audio = new Audio("./sounds/tom-3.mp3");
+		audio.play();
+	}
+	if(event.key === "l"){
+		var audio = new Audio("./sounds/tom-4.mp3");
+		audio.play();
+	}
+})
